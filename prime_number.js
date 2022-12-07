e14c7fd4efd02eed34ff0d136d4bda6a11b10618
RaghunandanJha <119413721+RaghunandanJha@users.noreply.github.com>
2022-12-07 10:51:41 +0530

    Create prime_number.js

---
+++ prime_number.js
@@ -0,0 +1,13 @@
+// Number is prime or not
+
+let num=12;
+let count=0;
+for(let i=1; i<=num; i++){
+  if(nm%i==0)
+    count++;
+}
+
+if(count==2)
+  console.log("Prime");
+else
+  console.log("Not Prime");
